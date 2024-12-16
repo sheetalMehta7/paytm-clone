ef65317904eacdd8b07c1ff3a57fa8b052eae9cc
sheetalMehta7 <sheetalmehta137@gmail.com>
2024-12-16 07:59:51 +0530

    add signin and login routes

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,3 +1,93 @@
-const signup = ()=>{
-    
-}
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/AsyncHandler";
+import { z } from "zod";
+import { User } from "../models/user.model";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const userRegistrationSchema = z.object({
+  firstname: z.string().trim(),
+  lastname: z.string().trim(),
+  email: z.string().email().trim(),
+  password: z.string().min(8).trim(),
+});
+
+const userLoginSchema = z.object({
+  email: z.string().email().trim(),
+  password: z.string().min(8).trim(),
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  // get data from the frontend
+  // do the validation
+  // if everything is okay check if the email id already exists
+  // add user to db.
+  // hash the password
+
+  try {
+    const userData = userRegistrationSchema.parse(req.body);
+
+    const userExists = User.findOne({ email: userData.email });
+    if (userExists) {
+      throw new ApiError(409, "Email already registered.");
+    }
+
+    const user = await User.create(userData);
+
+    const createdUser = await User.findById(user._id).select("-password");
+
+    if (!createdUser) {
+      throw new ApiError(500, "Something went wrong while regestering user.");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, createdUser, "User successfully resgistered.")
+      );
+  } catch (error) {
+    if (err instanceof z.ZodError) {
+      throw new ApiError(400, "Validation errors", error.errors);
+    } else {
+      throw new ApiError(500, "Something went wrong while regestering user.");
+    }
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  // get data from user
+  // validate the data
+  // check user email exists
+  // compare password
+  // authenticate the user
+  //return jwt
+  try {
+    const userData = userLoginSchema.parse(req.body);
+
+    const user = await User.findOne({ email: userData.email });
+    if (!user) {
+      throw new ApiError(400, "User not found.");
+    }
+
+    const correctPassword = await User.comparePassword(userData.password);
+
+    if (!correctPassword) {
+      throw new ApiError(401, "Incorrect password.");
+    }
+
+    const accessToken = user.generateAccessToken();
+
+    return res.status(200).json(
+      new ApiResponse(200, "User logged in successfully.", {
+        token: accessToken,
+      })
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new ApiError(400, "Validation errors", error.errors);
+    } else {
+      throw new ApiError(500, "Failed to login user");
+    }
+  }
+});
+
+export { registerUser };
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,24 +1,54 @@
 import { Model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstname: {
-        type: String,
-        require: true,
-        trim: true
+      type: String,
+      require: true,
+      trim: true,
     },
     lastname: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     email: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     password: {
-        type: String,
-        require: true
-    }
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password);
 });
 
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-export const User = Model.create('User', userSchema);
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = Model.create("User", userSchema);
